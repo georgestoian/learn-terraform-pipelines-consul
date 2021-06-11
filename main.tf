e76b6d65998eb35af207fbc2a86881b8c6d68da6
georgestoian <george.stoian31@gmail.com>
2021-06-11 12:23:24 +0300

    change name

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-consul"
+      name = "mihai-d-consul"
     }
   }
   required_providers {
